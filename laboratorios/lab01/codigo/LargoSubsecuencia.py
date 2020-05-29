7848732a5be4049c056acbe62ac97bafaa09a302
ssaldarris <60229701+ssaldarris@users.noreply.github.com>
2020-05-28 19:57:48 -0500

    Agregado lab1.2

---
+++ laboratorios/lab01/codigo/LargoSubsecuencia.py
@@ -0,0 +1,13 @@
+##LAB1
+##largosubsecuencia
+
+def largosubsecuencia(a, b):
+    if (len(a)==0) or (len(b)==0):
+        return 0
+    elif (a[0]==b[0]):
+        return 1 + largosubsecuencia(a[1:],b[1:])
+    else:
+        return max(largosubsecuencia(a[1:],b),largosubsecuencia(a,b[1:]))
+
+
+print(largosubsecuencia('adgjkl','afehjkl'))
